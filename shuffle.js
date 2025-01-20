67ef0b88bd20cd9078119e17144a27f5e9f65af3
Zettex <safaandsafa4@gmail.com>
2025-01-20 17:23:54 +0600

    can shuffle the colors

---
+++ shuffle.js
@@ -0,0 +1,18 @@
+let body = document.querySelector('body')
+
+function generateColor(){
+    let randomColor = '#'
+    for(let i = 1;i < 7; i++){
+        randomColor += Math.floor(Math.random()*10)
+    }
+    return randomColor
+}
+let intervalId;
+function changeColor(){
+    body.style.backgroundColor = generateColor()
+}
+
+function startShuffle(){
+    intervalId = setInterval(changeColor,1000)
+}    
+
